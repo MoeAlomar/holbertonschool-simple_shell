bfe9a7aed814d3e92aa2a4185560f148a6fc5d66
MoeAlomar <Mohammedahalomar@gmail.com>
2025-04-17 13:43:49 +0000

    Task 3 Fix

--- simple_shell.c
+++ simple_shell.c
@@ -1,33 +1,37 @@
-/* simple_shell.c */
 #include "main.h"
 #include <unistd.h>  /* for isatty() */
 
+/**
+ * main - simple shell entry point
+ *
+ * Return: Always 0 on success
+ */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
-    while (1)
-    {
-        /* only print prompt if we're interactive */
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
+	while (1)
+	{
+		/* show prompt only when running interactively */
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        nread = read_input(&line, &len);
-        if (nread == -1)  /* EOF (Ctrl+D) or error */
-        {
-            free(line);
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            exit(0);
-        }
+		nread = read_input(&line, &len);
+		if (nread == -1)  /* EOF or error */
+		{
+			free(line);
+			/* newline only in interactive mode */
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			exit(0);
+		}
 
-        remove_newline(line);
-        execute_command(line);
-    }
+		remove_newline(line);
+		execute_command(line);
+	}
 
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
-
