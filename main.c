173d371f136f130ba6f00aa5fd472a8ba9c7556d
KhaledCS1 <10688@holbertonstudents.com>
2025-04-18 17:28:53 +0000

    Rename files for better structure and clarity

--- main.c
+++ main.c
@@ -1,62 +1,6 @@
 #include "shell.h"
 
 int last_status = 0;
-/**
- * handle_command - Handles execution of a command
- * @args: Array of arguments
- */
-void handle_command(char **args)
-{
-	char *path_cmd = NULL;
-	pid_t pid;
-	int status;
-
-	if (args[0][0] == '/' || args[0][0] == '.')
-	{
-		if (access(args[0], X_OK) == 0)
-			path_cmd = strdup(args[0]);
-		else
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			last_status = 127;
-			return;
-		}
-	}
-	else
-	{
-		path_cmd = find_command_path(args[0]);
-		if (!path_cmd)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			last_status = 127;
-			return;
-		}
-	}
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork failed");
-		free(path_cmd);
-		last_status = 1;
-		return;
-	}
-
-	if (pid == 0)
-	{
-		execve(path_cmd, args, environ);
-		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-		free(path_cmd);
-		exit(127);
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			last_status = WEXITSTATUS(status);
-		free(path_cmd);
-	}
-}
 
 /**
  * main - Entry point for the simple shell
@@ -82,7 +26,14 @@ int main(void)
 		{
 			parse_arguments(line, args);
 			if (args[0] != NULL)
+			{
+				if (strcmp(args[0], "exit") == 0)
+				{
+					free(line);
+					exit(last_status);
+				}
 				handle_command(args);
+			}
 		}
 
 		if (isatty(STDIN_FILENO))
