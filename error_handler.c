2c60601ac151165f433ddd0c2e54a6b4a97052f8
KhaledCS1 <10688@holbertonstudents.com>
2025-04-18 23:39:35 +0000

    Fixed and added

--- error_handler.c
+++ error_handler.c
@@ -1,12 +1,8 @@
 #include "shell.h"
 
-/**
-* print_error - prints error message when command is not found
-* @cmd: the command that failed
-*/
 void print_error(char *cmd)
 {
-write(STDERR_FILENO, "./hsh: ", 7);
-write(STDERR_FILENO, cmd, _strlen(cmd));
-write(STDERR_FILENO, ": command not found\n", 20);
+    write(STDERR_FILENO, "./hsh: 1: ", 10);
+    write(STDERR_FILENO, cmd, _strlen(cmd));
+    write(STDERR_FILENO, ": not found\n", 12);
 }
