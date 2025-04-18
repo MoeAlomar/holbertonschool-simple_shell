47db1b5e9d893a731f6b1e56d22a737b3ced4344
KhaledCS1 <10688@holbertonstudents.com>
2025-04-18 21:33:26 +0000

    Fixed execute.c

--- execute.c
+++ execute.c
@@ -1,20 +1,20 @@
 #include "shell.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "unistd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 /**
  * find_command_path - Searches for a command in the PATH environment variable
  * @command: The command to search for
  *
- * Return: Full path to command if found, otherwise NULL
+ * Return: A pointer to a string containing the full path if found, otherwise NULL
  */
 char *find_command_path(char *command)
 {
 	char *path = getenv("PATH");
-	char *path_copy = NULL, *token = NULL;
+	char *path_copy, *token;
 	char full_path[1024];
 
 	if (!path)
@@ -25,13 +25,13 @@ char *find_command_path(char *command)
 		return (NULL);
 
 	token = strtok(path_copy, ":");
-	while (token != NULL) {
-		{
+	while (token != NULL)
+	{
 		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
 		if (access(full_path, X_OK) == 0)
 		{
 			free(path_copy);
-			return	(strdup(full_path));
+			return (strdup(full_path));
 		}
 		token = strtok(NULL, ":");
 	}
@@ -40,70 +40,67 @@ char *find_command_path(char *command)
 }
 
 /**
- * handle_command - Handles execution of a command
- * @args: Array of arguments (command and its parameters)
+ * execute_command - Forks and executes the command using execve
+ * @path: Full path to the command
+ * @args: Array of arguments
  *
  * Return: Nothing
  */
+void execute_command(char *path, char **args)
+{
+	pid_t pid;
+	int status;
 
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+}
+
+/**
+ * handle_command - Handles command input, resolves path and executes
+ * @args: Array of arguments passed to the shell
+ *
+ * Return: Nothing
+ */
 void handle_command(char **args)
 {
 	char *path_cmd = NULL;
 
-	if (args[0][0] == '/' || args[0][0] == '.') {
-		{
-			if (access(args[0], X_OK) == 0) {
-				path_cmd = strdup(args[0]);
-			}
-			else
-			{	
-				fprintf(stderr, "./hsh: 1: %s; No such file or directory\n", args[0]);
-				last_status = 127;
-				return;
-			}
-		}
+	if (args[0][0] == '/' || args[0][0] == '.')
+	{
+		if (access(args[0], X_OK) == 0)
+			path_cmd = strdup(args[0]);
 		else
 		{
-			path_cmd = find_command_path(args[0]);
-			if (!path_cmd) {
-
-				fprintf(stderr, "./hsh: 1: No such file or directory\n", args[0]);
-				last_status = 127;
-				return;
-			}	
-		}	
-		
-		execute_command(path_cmd, args);
-			free(path_cmd);
-	}
-	/**
-	* execute_command - Fork and run a command using execve
-	* @path: Full path to the command
-	* @args: Array of arguments
-	*
-	* Return: Nothing
-	*/
-	void execute_command(char *path, char **args) {
-		pid_t pid;
-		int status;
-
-		pid = fork();
-		if (pid == -1) {
-			perror("fork");
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 			return;
 		}
-
-		if (pid == 0) {
-			if (execve(path, args, environ) == -1) {
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-	       	{
-			waitpid(pid, &status, 0);
-			if (WIFEXITED(status))
-				last_status = WEXITSTATUS(status);
-	
 	}
+	else
+	{
+		path_cmd = find_command_path(args[0]);
+		if (!path_cmd)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			return;
+		}
+	}
+
+	execute_command(path_cmd, args);
+	free(path_cmd);
 }
