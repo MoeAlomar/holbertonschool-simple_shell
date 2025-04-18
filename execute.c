9e875df30a1545fcee1f5cb3b4a96f435348af7e
KhaledCS1 <10688@holbertonstudents.com>
2025-04-18 21:42:32 +0000

    Fixed execute.c

--- execute.c
+++ execute.c
@@ -1,9 +1,18 @@
 #include "shell.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern char **environ;
 
 /**
  * execute_command - Fork and run a command
  * @path: full path to the command
  * @args: array of arguments
+ *
+ * Return: Nothing
  */
 void execute_command(char *path, char **args)
 {
