10357be24be0695d061e4d083df3163a22797a2e
MoeAlomar <Mohammedahalomar@gmail.com>
2025-04-20 21:20:03 +0000

    Task 5 Fix

--- main.c
+++ main.c
@@ -1,66 +1,73 @@
 #include "shell.h"
 
 /**
-* main - Entry point for shell
-* @argc: Argument count (unused)
-* @argv: Argument vector
-* @env: Environment variables
-*
-* Return: Always 0
-*/
+ * main - Entry point for shell
+ * @argc: Argument count (unused)
+ * @argv: Argument vector
+ * @env: Environment variables
+ *
+ * Return: last exit status of executed command or exit code on `exit`
+ */
 int main(int argc, char **argv, char **env)
 {
-char *line = NULL;
-char **args = NULL;
-int status = 1;
-(void)argc;
-(void)argv;
+    char *line = NULL;
+    char **args = NULL;
+    int status = 0; /* store last command exit status */
 
-while (1)
-{
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "$ ", 2);
+    (void)argc;
+    (void)argv;
 
-line = read_line();
-if (!line)
-{
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "\n", 1);
-break;
-}
+    while (1)
+    {
+        /* display prompt if interactive */
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "$ ", 2);
 
-args = split_line(line);
-if (!args || !args[0])
-{
-free(line);
-free_args(args);
-continue;
-}
+        line = read_line();
+        if (!line)
+        {
+            /* handle EOF */
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
 
-if (_strcmp(args[0], "exit") == 0)
-{
-    free(line);
-    free_args(args);
+        args = split_line(line);
+        if (!args || !args[0])
+        {
+            free(line);
+            free_args(args);
+            continue;
+        }
 
-    if (args[1])
-        exit(atoi(args[1]));
-    else
-        exit(status);
-}
+        /* built-in: exit */
+        if (_strcmp(args[0], "exit") == 0)
+        {
+            int exit_code = status;
 
-else if (_strcmp(args[0], "env") == 0)
-{
-print_env();
-status = 0;
-}
-else
-{
-status = execute(args, env);
-}
+            /* if user provided an exit code, use it */
+            if (args[1])
+                exit_code = atoi(args[1]);
 
-free(line);
-free_args(args);
-}
+            free(line);
+            free_args(args);
+            exit(exit_code);
+        }
+        /* built-in: env */
+        else if (_strcmp(args[0], "env") == 0)
+        {
+            print_env();
+            status = 0;
+        }
+        else
+        {
+            /* execute external command and save its exit status */
+            status = execute_args(args, env);
+        }
 
-return (status);
+        free(line);
+        free_args(args);
+    }
+
+    return (status);
 }
